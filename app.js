2bc3b773db0cc326de87d78975e0cdef7efeada2
Minkshiydv <raoronika236@gmail.com>
2025-08-27 17:09:20 +0530

    update app.js file

--- app.js
+++ app.js
@@ -104,7 +104,11 @@ cookie: {
 
     passport.serializeUser(User.serializeUser());
     passport.deserializeUser(User.deserializeUser());
-
+  
+    app.use((req,res,next) => {
+  res.locals.currUser = req.user;
+  next();
+});
 
 app.use((req , res , next) => {
   res.locals.success = req.flash("success");
